23960abd5975dcd72818562a5c48f7b8eae6de77
Amogh Lokhande <123250280+Amogh052003@users.noreply.github.com>
2025-07-07 13:20:28 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          red: "#FF0000",
-          black: "#0F0F0F",
-          white: "#FFFFFF",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
