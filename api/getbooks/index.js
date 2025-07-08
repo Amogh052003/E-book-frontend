7b0fda03ed2e1dc27b1e1d186c8a0a0d1a7dbe50
Amogh Lokhande <123250280+Amogh052003@users.noreply.github.com>
2025-07-08 13:35:24 +0530

    Update index.js

--- api/getbooks/index.js
+++ api/getbooks/index.js
@@ -1,1 +1,24 @@
+module.exports = async function (context, req) {
+  const books = [
+    {
+      id: 1,
+      title: "The Cloud Handbook",
+      author: "Jane Doe",
+      price: 299
+    },
+    {
+      id: 2,
+      title: "Azure Essentials",
+      author: "John Smith",
+      price: 399
+    }
+  ];
 
+  context.res = {
+    status: 200,
+    body: books,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+};
