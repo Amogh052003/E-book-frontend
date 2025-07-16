3fca405a3794897bca449c0d4e939c6a099be6c6
Amogh Lokhande <123250280+Amogh052003@users.noreply.github.com>
2025-07-16 13:46:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
-import { CartProvider } from "./context/CartContext";
+
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </React.StrictMode>
+  <MsalProvider instance={msalInstance}>
+    <App />
+  </MsalProvider>
 );
