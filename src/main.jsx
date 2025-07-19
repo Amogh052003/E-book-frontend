20f77aa11f2f2c41ee28261abfbfb416a2267d82
Amogh Lokhande <123250280+Amogh052003@users.noreply.github.com>
2025-07-19 14:09:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 
 import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from "./context/AuthContext"; // <-- Add this
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
@@ -13,9 +14,11 @@ const msalInstance = new PublicClientApplication(msalConfig);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <AuthProvider> {/* <-- Wrap your app with AuthProvider */}
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
     </MsalProvider>
   </React.StrictMode>
 );
